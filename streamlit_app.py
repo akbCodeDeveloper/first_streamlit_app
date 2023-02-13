55175510c9345ba1ce9bcfda98e9fc0f9afc4bc2
akbCodeDeveloper <125243053+akbCodeDeveloper@users.noreply.github.com>
2023-02-13 12:40:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header('My Breakfast Menu')
-streamlit.text('\N{bowl with spoon} Omega 3 & Blueberry Meal' ) 
+streamlit.text('\N {bowl with spoon} Omega 3 & Blueberry Meal' ) 
 streamlit.text('Kale , Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
