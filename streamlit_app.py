858692302070e0bf3da155786db839800f74ae11
akbCodeDeveloper <125243053+akbCodeDeveloper@users.noreply.github.com>
2023-02-13 12:20:55 +0530

    Update streamlit_app.py
    Added a Header and 
    2 TextLine for Omega and Kale

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header('My Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Meal' ) 
+streamlit.text('Kale , Spinach & Rocket Smoothie')
