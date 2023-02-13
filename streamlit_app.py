4815e867b5709465dd5253890d1ce242408d8a05
akbCodeDeveloper <125243053+akbCodeDeveloper@users.noreply.github.com>
2023-02-13 12:39:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header('https://unicode.org/emoji/charts/emoji-list.html#1f4aa My Breakfast Menu')
-streamlit.text(' Omega 3 & Blueberry Meal' ) 
+streamlit.header('My Breakfast Menu')
+streamlit.text('\N{bowl with spoon} Omega 3 & Blueberry Meal' ) 
 streamlit.text('Kale , Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
